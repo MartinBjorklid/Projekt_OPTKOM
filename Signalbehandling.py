33f7248e056b70b84a8ba51e57bc57470062bca0
MartinBjorklid <marbj628@student.liu.se>
2026-09-07 10:56:06 +0200

    Add files via upload

---
+++ Signalbehandling.py
@@ -0,0 +1,18 @@
+import 
+import
+import
+
+
+def readfile(filename):
+    return a string or smth
+
+
+def hoffman(string)
+
+def to_bin(strin or smth):
+    return binary_number
+
+
+
+if __name__ == '__main_-':
+    readfile(filename=)
