f2173f92f1fcc7e2217a4db8a3d7f04acf7faa22
MartinBjorklid <marbj628@student.liu.se>
2026-09-07 11:11:10 +0200

    Update Signalbehandling.py
    Added a function to convert integers to binary and updated the hoffman function definition.

--- Signalbehandling.py
+++ Signalbehandling.py
@@ -7,10 +7,25 @@ def readfile(filename):
     return a string or smth
 
 
-def hoffman(string)
+def hoffman(string) #Kolla frekvens och koda till tal i ökande ordning
+
+def get_binary(x: int) -> str:       #gör om talen till binär       
+    """ Ex3: Returns the binary representation of x """
+    if x<0:
+        return "-" + get_binary(-x) #Om det är ett negativt tall, gör det för det positiva
+    
+    if x==0:
+        return "0" #Om x är noll så blir det noll
+    if x==1:
+        return "1" #Om x är ett så blir det en etta
+    
+    return get_binary(x//2) + str(x%2) #Kör talet heltal delat på 2, skriv ut det som blir resten, en etta ellet nolla
+
+    pass
+
 
 def to_bin(strin or smth):
-    return binary_number
+    return binary_number #gör till en utdata
 
 
 
